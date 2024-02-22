67dbd9065d292e74082effcb9c91b57c5b34b7dc
PetiSui <minittf@gmail.com>
2024-02-22 19:03:07 +0100

    Changed disliked icon to empty heart. Added Set to localStorage check to improve speed in lookups and deletions

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -2,6 +2,7 @@ import {React, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeartBroken } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faEmptyHeart} from "@fortawesome/free-regular-svg-icons";
 
 const LikeButton = ({id}) => {
 
@@ -10,16 +11,21 @@ const LikeButton = ({id}) => {
 
   let [likedPlaces, setLikedPlaces] = useState(likedEstablishments);
   let [clicked, setClicked] = useState(false);
+  console.log("Clicked " + clicked);
+
+  let set = new Set(likedEstablishments);
+  console.log(set);
   
-  
-  
-  
-  let found = likedEstablishments.includes(id);
+  // let found = likedEstablishments.includes(id);
+  let found = set.has(id);
   console.log(found);
 
   const handleLiked = () => {
-    found ? likedEstablishments = likedEstablishments.filter(item => item != id) : likedEstablishments.push(id);
-    localStorage.setItem('likedEstablishments', JSON.stringify(likedEstablishments))
+    // found ? likedEstablishments = likedEstablishments.filter(item => item != id) : likedEstablishments.push(id);
+    found ? set.delete(id) : set.add(id);
+    // localStorage.setItem('likedEstablishments', JSON.stringify(likedEstablishments));
+    likedEstablishments = JSON.stringify(Array.from(set));
+    localStorage.setItem('likedEstablishments', likedEstablishments)
     setLikedPlaces(likedEstablishments);
   };
 
@@ -31,18 +37,17 @@ const LikeButton = ({id}) => {
         setClicked(true);
         handleLiked();
       }}
-      clicked={clicked}
+      clicked={clicked.toString()}
     >
       {found ? (
         <FontAwesomeIcon
-          icon={faHeartBroken}
+          icon={faHeart}
           className="like"
           size="xl"
-          // data-liked={clicked.toString()}
         ></FontAwesomeIcon>
       ) : (
         <FontAwesomeIcon
-          icon={faHeart}
+          icon={faEmptyHeart}
           className="like"
           size="xl"
           data-liked={clicked.toString()}
